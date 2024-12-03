b240b8986fc51cc5a25c98464692379cf082a8e8
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-03 16:07:40 +0530

    Add files via upload

---
+++ bevod.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class bevod{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        int a=S.nextInt();
+        if((a&1)==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+        }
+    }
