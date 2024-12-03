284fdf0ce8195692edd77552299ce6462f561540
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-03 10:24:13 +0530

    Add files via upload

---
+++ Assign.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Assign{
+    public static void main(String args[]){
+        Scanner S=new Scanner(System.in);
+        int a,b;
+        a=S.nextInt();
+        b=S.nextInt();
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+    }
+}
